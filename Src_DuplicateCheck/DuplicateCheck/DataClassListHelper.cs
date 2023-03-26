72807240ff49cf97705c9198a1115d3d082416ab
ukami <ukami@interserve.co.jp>
2023-03-26 18:38:05 +0900

    1

---
+++ Src_DuplicateCheck/DuplicateCheck/DataClassListHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace DuplicateCheck
+{
+    public static class DataClassListHelper
+    {
+        /// <summary>
+        /// データクラスのリストに、重複しているId/Codeがあるかチェックする
+        /// </summary>
+        /// <returns>true;重複無し　false;重複有り</returns>
+        public static bool IsDuplicateId_Ptn1(List<DataClass> dataList)
+        {
+            if (dataList.Where(x1 => x1.Id != 0)
+                .Select(x2 => x2.Id)
+                .GroupBy(x3 => x3)
+                .Any(x4 => x4.Count() > 1))
+                return false;    //重複有り
+
+            return true;   //重複無し
+        }
+
+        /// <summary>
+        /// データクラスのリストに、重複しているIdがあるかチェックする
+        /// </summary>
+        /// <returns>
+        /// true;重複無し
+        /// false;重複有り
+        /// 重複したIdのリスト。
+        /// </returns>
+        public static (bool, List<DataClass>?) IsDuplicateId_Ptn2(List<DataClass> dataList)
+        {
+            var duplicateListId = dataList.Where(x1 => x1.Id != 0)
+                .Select(x2 => x2.Id)
+                .GroupBy(x3 => x3)
+                .Where(x4 => x4.Count() > 1)
+                .Select(x5 => x5.Key).ToList();
+
+
+            if (duplicateListId.Count() > 0)
+            {
+                return (false, dataList.Where(x6 => duplicateListId.Select(Id => Id).Contains(x6.Id)).ToList());    //重複有り
+            }
+
+            return (true, null) ;   //重複無し
+        }
+
+        /// <summary>
+        /// データクラスのリストに、重複しているId/Codeがあるかチェックする
+        /// </summary>
+        /// <returns>true;重複無し　false;重複有り</returns>
+        public static bool IsDuplicateCode_Ptn1(List<DataClass> dataList)
+        {
+            if (dataList.Where(x1 => !string.IsNullOrEmpty(x1.Code))
+                .Select(x2 => x2.Code)
+                .GroupBy(x3 => x3)
+                .Any(x4 => x4.Count() > 1))
+                return false;    //重複有り
+
+            return true;   //重複無し
+        }
+
+        /// <summary>
+        /// データクラスのリストに、重複しているCodeがあるかチェックする
+        /// </summary>
+        /// <returns>
+        /// true;重複無し
+        /// false;重複有り
+        /// 重複したIdのリスト。
+        /// </returns>
+        public static (bool, List<DataClass>?) IsDuplicateCode_Ptn2(List<DataClass> dataList)
+        {
+            var duplicateListCode = dataList.Where(x1 => !string.IsNullOrEmpty(x1.Code))
+                .Select(x2 => x2.Code)
+                .GroupBy(x3 => x3)
+                .Where(x4 => x4.Count() > 1)
+                .Select(x5 => x5.Key).ToList();
+
+            if (duplicateListCode.Count() > 0)
+                return (false, dataList.Where(x6 => duplicateListCode.Select(Code => Code).Contains(x6.Code)).ToList());    //重複有り
+
+            return (true, null);   //重複無し
+        }
+
+    }
+}
---
+++ Src_DuplicateCheck/DuplicateCheckTests/DataClassListHelperTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DuplicateCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateCheck.Tests
+{
+    [TestClass()]
+    public class DataClassListHelperTests
+    {
+        private List<DataClass> DataClassList1;
+        private List<DataClass> DataClassList2;
+        private List<DataClass> DataClassList3;
+        private List<DataClass> DataClassList4;
+
+        public DataClassListHelperTests()
+        {
+            DataClassList1 = new List<DataClass>
+            {
+                new DataClass() { Id = 1, Code = "001", Value = "テスト１" },
+                new DataClass() { Id = 2, Code = "002", Value = "テスト２" },
+                new DataClass() { Id = 3, Code = "003", Value = "テスト３" }
+            };
+
+            DataClassList2 = new List<DataClass>
+            {
+                new DataClass() { Id = 1, Code = "001", Value = "テスト１" },
+                new DataClass() { Id = 2, Code = "002", Value = "テスト１" },
+                new DataClass() { Id = 3, Code = "003", Value = "テスト１" }
+            };
+
+            DataClassList3 = new List<DataClass>
+            {
+                new DataClass() { Id = 1, Code = "001", Value = "テスト１" },
+                new DataClass() { Id = 2, Code = "002", Value = "テスト２" },
+                new DataClass() { Id = 1, Code = "003", Value = "テスト３" }
+            };
+
+            DataClassList4 = new List<DataClass>
+            {
+                new DataClass() { Id = 1, Code = "001", Value = "テスト１" },
+                new DataClass() { Id = 2, Code = "002", Value = "テスト２" },
+                new DataClass() { Id = 3, Code = "001", Value = "テスト３" }
+            };
+        }
+
+
+        [TestMethod()]
+        public void IsDuplicateId_Ptn1Test()
+        {
+            Assert.IsTrue(DataClassListHelper.IsDuplicateId_Ptn1(DataClassList1));
+            Assert.IsTrue(DataClassListHelper.IsDuplicateId_Ptn1(DataClassList2));
+            Assert.IsFalse(DataClassListHelper.IsDuplicateId_Ptn1(DataClassList3));
+            Assert.IsTrue(DataClassListHelper.IsDuplicateId_Ptn1(DataClassList4));
+        }
+
+        [TestMethod()]
+        public void IsDuplicateId_Ptn2Test()
+        {
+            var result1 = DataClassListHelper.IsDuplicateId_Ptn2(DataClassList1);
+            Assert.IsTrue(result1.Item1);
+            Assert.IsTrue(result1.Item2?.Count() == null);
+
+            var result2 = DataClassListHelper.IsDuplicateId_Ptn2(DataClassList2);
+            Assert.IsTrue(result2.Item1);
+            Assert.IsTrue(result2.Item2?.Count() == null);
+
+            var result3 = DataClassListHelper.IsDuplicateId_Ptn2(DataClassList3);
+            Assert.IsFalse(result3.Item1);
+            Assert.IsTrue(result3.Item2?.Count() > 0);
+
+            var result4 = DataClassListHelper.IsDuplicateId_Ptn2(DataClassList4);
+            Assert.IsTrue(result4.Item1);
+            Assert.IsTrue(result4.Item2?.Count() == null);
+        }
+
+        [TestMethod()]
+        public void IsDuplicateCode_Ptn1Test()
+        {
+            Assert.IsTrue(DataClassListHelper.IsDuplicateCode_Ptn1(DataClassList1));
+            Assert.IsTrue(DataClassListHelper.IsDuplicateCode_Ptn1(DataClassList2));
+            Assert.IsTrue(DataClassListHelper.IsDuplicateCode_Ptn1(DataClassList3));
+            Assert.IsFalse(DataClassListHelper.IsDuplicateCode_Ptn1(DataClassList4));
+        }
+
+        [TestMethod()]
+        public void IsDuplicateCode_Ptn2Test()
+        {
+            var result1 = DataClassListHelper.IsDuplicateCode_Ptn2(DataClassList1);
+            Assert.IsTrue(result1.Item1);
+            Assert.IsTrue(result1.Item2?.Count() == null);
+
+            var result2 = DataClassListHelper.IsDuplicateCode_Ptn2(DataClassList2);
+            Assert.IsTrue(result2.Item1);
+            Assert.IsTrue(result2.Item2?.Count() == null);
+
+            var result3 = DataClassListHelper.IsDuplicateCode_Ptn2(DataClassList3);
+            Assert.IsTrue(result3.Item1);
+            Assert.IsTrue(result3.Item2?.Count() == null);
+
+            var result4 = DataClassListHelper.IsDuplicateCode_Ptn2(DataClassList4);
+            Assert.IsFalse(result4.Item1);
+            Assert.IsTrue(result4.Item2?.Count() > 0);
+        }
+    }
+}
